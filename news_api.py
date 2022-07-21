590ac56a8ae724d15eb9642ade8b9932a4bb5626
arroeldo <tejasssawant027@gmail.com>
2022-07-21 17:45:20 +0530

    Added new function to news_api

--- news_api.py
+++ news_api.py
@@ -24,3 +24,24 @@ def top_headlines(country: str, api_Key: str):
         lst.append(article_dict)
     
     return lst
+
+# the following searches for news articles for a specified topic 
+def news(api_key: str, topic: str, domain: str = None, from_date: str = None, to_date: str = None,
+         language: str = "en",
+         sortby: str = "publishedAt"):
+    lst = []
+    endpoint = "https://newsapi.org/v2/everything"
+    parameters = {"apiKey": api_key, "q": topic, "domains": domain, "from": from_date, "to": to_date,
+                  "language": language, "sortBy": sortby}
+    request = requests.get(endpoint, params=parameters)
+    articles_list = request.json()['articles']
+
+    for article in articles_list:
+        article_dict = {
+            'source': article['source']['name'],
+            'title': article['title'],
+            'link': article['url'],
+            'date': article['publishedAt'][:10]
+        }
+        lst.append(article_dict)
+    return lst
