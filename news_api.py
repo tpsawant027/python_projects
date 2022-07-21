c38a9e5e875372a16a9e12aeef4bbbf552e9e5e0
arroeldo <tejasssawant027@gmail.com>
2022-07-21 14:40:34 +0530

    news_api added

---
+++ news_api.py
@@ -0,0 +1,26 @@
+# program to fetch top news headlines for a specified country
+# results are from https://newsapi.org/
+
+import requests
+
+# the function returns a list of dictionaries each dictionary representing information about an article.
+# each dictionary has 3 keys (source, title, link/url)
+# the function takes two parameters a country code and an api_key
+
+def top_headlines(country: str, api_Key: str):
+    lst = []
+
+    endpoint = "https://newsapi.org/v2/top-headlines"
+    parameters = {"country": country, "apiKey": api_Key}
+    request = requests.get(endpoint, params=parameters)
+    articles_list = request.json()['articles']
+
+    for article in articles_list:
+        article_dict = {
+            'source': article['source']['name'],
+            'title': article['title'],
+            'link': article['url']
+        }
+        lst.append(article_dict)
+    
+    return lst
